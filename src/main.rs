8acd133b2486671aef77f9573ebf3e7b0cca26a6
110141 <110141@gltech.org>
2025-09-16 12:31:30 -0400

    Add choice of selectors

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,18 @@ use druid::widget::{Align, Button, Flex, Label, TextBox, WidgetExt};
 use druid::{AppLauncher, Data, Env, Lens, Widget, WindowDesc};
 use scraper::{Html, Selector};
 #[derive(Clone, Data, Lens)]
-struct HelloState {
-    name: String,
+struct ScraperState {
+    url: String,
+    selector: String
 }
 
-fn scrape(url: String){
+fn scrape(url: String, selector: String){
     let response = reqwest::blocking::get(
         url).unwrap().text().unwrap();
 
     let doc_body = Html::parse_document(&response);
 
-    let title = Selector::parse("td").unwrap();
+    let title = Selector::parse(&*selector).unwrap();
 
     for title in doc_body.select(&title) {
         let titles = title.text().collect::<Vec<_>>();
@@ -28,8 +29,9 @@ fn main() {
         .title("Hyperion")
         .window_size((400.0, 400.0));
 
-    let stateInit = HelloState{
-        name: " ".into(),
+    let stateInit = ScraperState{
+        url: "".into(),
+        selector: "".into()
     };
 
     AppLauncher::with_window(window)
@@ -37,23 +39,29 @@ fn main() {
         .expect("Failed to initialize Hyperion");
 }
 
-fn  buildRootWidget() -> impl Widget<HelloState> {
-    let label = Label::new(|data : &HelloState, _env: &Env| format!("Pasting scraped text into console from {}", data.name));
+fn  buildRootWidget() -> impl Widget<ScraperState> {
+    let label = Label::new(|data : &ScraperState, _env: &Env| format!("Pasting scraped text into console from {} elements in {}", data.selector, data.url));
 
-    let textBox = TextBox::new()
-        .with_placeholder("Test")
+    let urlBox = TextBox::new()
+        .with_placeholder("URL")
         .fix_width(200.0)
-        .lens(HelloState::name);
+        .lens(ScraperState::url);
+
+    let selectorBox = TextBox::new()
+        .with_placeholder("HTML Selector")
+        .fix_width(200.0)
+        .lens(ScraperState::selector);
 
     let button = Button::new("Scrape")
-        .on_click(|_, data: &mut HelloState, _env| {
-            scrape(data.name.clone())
+        .on_click(|_, data: &mut ScraperState, _env| {
+            scrape(data.url.clone(), data.selector.clone())
         });
 
     let layout = Flex::column()
         .with_child(label)
         .with_spacer(10.0)
-        .with_child(textBox)
+        .with_child(urlBox)
+        .with_child(selectorBox)
         .with_child(button);
     Align::centered(layout)
 }
