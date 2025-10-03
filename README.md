f0fcf58388098e88df5b4194f4345075f26f9ed9
110141 <110141@gltech.org>
2025-10-03 08:17:55 -0400

    Display output in window, instead of console

--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 Hyperion is a lightweight Rust program that isolates HTML elements' text from websites and prints them to your console.
 Hyperion is developed for Windows, though it can likely be compiled to run on Linux based operating systems.
 
-This was built by a 16 year old in a few hours to learn the basics of Rust, expect many issues.
+This is my first Rust project, expect many issues.
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,50 @@
+#![windows_subsystem = "windows"]
 #![allow(non_snake_case)]
-//#![windows_subsystem = "windows"]
 
-use xilem::view::{button, flex, label, textbox};
+use xilem::view::{button, flex, label, portal, textbox};
 use xilem::{EventLoop, WidgetView, Xilem};
 use winit::error::EventLoopError;
 use scraper::{Html, Selector};
 
 #[derive(Default)]
-struct ScraperState {
+struct AppData {
     url: String,
-    selector: String
+    selector: String,
+    output: String,
 }
 
-fn scrape(url: String, selector: String){
+fn scrape(url: String, selector: String) -> String {
     let response = reqwest::blocking::get(
         url).unwrap().text().unwrap();
-
     let doc = Html::parse_document(&response);
-
     let title = Selector::parse(&*selector).unwrap();
+    let mut output = String::new();
 
     for title in doc.select(&title) {
         let titles = title.text().collect::<Vec<_>>();
-        println!("{}", titles[0])
+        println!("{}", titles[0]);
+        output.push_str("\r\n");
+        output.push_str(titles[0])
     }
+    output
 }
 
-fn appLogic(data:&mut ScraperState) -> impl WidgetView<ScraperState> + use<>{
+fn appLogic(data:&mut AppData) -> impl WidgetView<AppData> + use<>{
     flex((
             label(format!("Pasting scraped text into console from {} elements in {}", data.selector, data.url)),
-            textbox(data.url.clone(), |data: &mut ScraperState, url|{
+            textbox(data.url.clone(), |data: &mut AppData, url|{
                 data.url = url
             }),
-            textbox(data.selector.clone(), |data: &mut ScraperState, selector|{
+            textbox(data.selector.clone(), |data: &mut AppData, selector|{
                 data.selector = selector
             }),
-            button("Scrape", |data: &mut ScraperState| scrape(data.url.clone(), data.selector.clone())),
+            button("Scrape", |data: &mut AppData| data.output =  scrape(data.url.clone(), data.selector.clone())),
+            portal(label(format!("{}", data.output)))
         ))
 }
 
 fn main() -> Result<(), EventLoopError> {
-    let window = Xilem::new(ScraperState::default(), appLogic);
+    let window = Xilem::new(AppData::default(), appLogic);
     window.run_windowed(EventLoop::with_user_event(), "Hyperion".into())?;
     Ok(())
 }
